f55eccc3c1a133b14c5dc1ef0bae26902a1a3231
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-20 20:25:22 +0200

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -1,7 +1,7 @@
 #Distributed under GNU General Public License Number 3 by Bastien Caillot and Bastcoin Core Developers
 
 from blake3 import blake3  #Blake3 hash function in Python language
-import binascii, struct, array, os, time, sys, optparse
+import binascii, struct, array, os, time, sys, optparse, codecs
 
 def main():
   options = get_args()
@@ -53,7 +53,7 @@ def create_input_script(psz_timestamp):
   #use OP_PUSHDATA1 if required
   if len(psz_timestamp) > 76: psz_prefix = '4c'
 
-  script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).encode('hex')
+  script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).codecs.encode()
   print (script_prefix + psz_timestamp.encode('hex'))
   return (script_prefix + psz_timestamp.encode('hex')).decode('hex')
 
