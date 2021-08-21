c3bb755af898e7d56b4dd7c623a2f53a0c2b140d
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-21 16:26:53 +0200

    👁 I’m tired of not understanding anything, every time I make a commit in this repository, I’ll put an emoji
    (

--- genesis.py
+++ genesis.py
@@ -56,7 +56,7 @@ def create_input_script(psz_timestamp):
 
   script_prefix = '04ffff001d0104' + psz_prefix +  codecs.encode(chr(len(psz_timestamp)) , 'hex')
   print ("script_prefix +  codecs.encode(psz_timestamp , 'hex')")
-  return codecs.decode(script_prefix + codecs.encode(psz_timestamp, 'hex')) , 'hex')
+  return codecs.decode(script_prefix + codecs.encode(psz_timestamp, 'hex') , 'hex')
 
 
 def create_output_script(pubkey):
