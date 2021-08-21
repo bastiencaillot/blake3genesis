4b6d86d0c905e2cef4633efacbfd377bf8442a42
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-21 20:28:05 +0200

    🦄
    🦄🦄🦄🦄

--- genesis.py
+++ genesis.py
@@ -54,7 +54,7 @@ def create_input_script(psz_timestamp):
   #use OP_PUSHDATA1 if required
   if len(psz_timestamp) > 76: psz_prefix = '4c'
 
-  script_prefix = '04ffff001d0104' + psz_prefix +  binascii.hexlify(chr(len(psz_timestamp))
+  script_prefix = '04ffff001d0104' + psz_prefix +  binascii.hexlify(chr(len(psz_timestamp)))
   print ("script_prefix +  binascii.hexlify(psz_timestamp)")
   return binascii.unhexlify(script_prefix + binascii.hexlify(psz_timestamp))
 
