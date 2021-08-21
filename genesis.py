2cff47beb09c581c4594f0062709f4d17d346320
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-21 16:15:59 +0200

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -55,8 +55,8 @@ def create_input_script(psz_timestamp):
   if len(psz_timestamp) > 76: psz_prefix = '4c'
 
   script_prefix = '04ffff001d0104' + psz_prefix + hex(ord(chr(len(psz_timestamp)))
-  print (script_prefix + binascii.hexlify(psz_timestamp))
-  return unhexlify(script_prefix + psz_timestamp)
+  print (script_prefix + hex(ord(psz_timestamp)))
+  return codecs.decode(script_prefix + psz_timestamp , 'hex')
 
 
 def create_output_script(pubkey):
