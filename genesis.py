1e899c9ed4160051bb1a2c9278bba49d104e5db6
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-21 18:01:18 +0200

    ( 🦄

--- genesis.py
+++ genesis.py
@@ -55,7 +55,7 @@ def create_input_script(psz_timestamp):
 
   script_prefix = '04ffff001d0104' + psz_prefix +  binascii.hexlify(chr(len(psz_timestamp)))
   print ("script_prefix +  binascii.hexlify(psz_timestamp)")
-  return binascii.unhexlify((script_prefix + binascii.hexlify(psz_timestamp))
+  return binascii.unhexlify(script_prefix + binascii.hexlify(psz_timestamp))
 
 
 def create_output_script(pubkey):
