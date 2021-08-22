3b06321a1572112d4d7a033eaed807fea2159c88
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-22 21:15:20 +0200

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -55,7 +55,7 @@ def create_input_script(psz_timestamp):
     
   script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).encode().hex()
   print (script_prefix + psz_timestamp.encode().hex())
-  decoder = bytes.fromhex(script_prefix + psz_timestamp.encode().hex()).decode()
+  decoder = bytes.fromhex(script_prefix + psz_timestamp.encode().hex()).decode('utf-16')
   return decoder
 
   
