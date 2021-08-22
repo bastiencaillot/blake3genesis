5289e673f7b18f927205ad6c81de613f84ea6bc1
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-22 20:25:18 +0200

    Unicode ;)

--- genesis.py
+++ genesis.py
@@ -53,9 +53,9 @@ def create_input_script(psz_timestamp):
   #use OP_PUSHDATA1 if required
   if len(psz_timestamp) > 76: psz_prefix = '4c'
     
-  script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).encode('ascii').hex()
-  print (script_prefix + psz_timestamp.encode('ascii').hex())
-  return (script_prefix + psz_timestamp.encode('ascii').hex()).decode('ascii').hex()
+  script_prefix = '04ffff001d0104' + psz_prefix + chr(len(psz_timestamp)).encode().hex()
+  print (script_prefix + psz_timestamp.encode().hex())
+  return (script_prefix + psz_timestamp.encode().hex()).decode().hex()
 
   
 
