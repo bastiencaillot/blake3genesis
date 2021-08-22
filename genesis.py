137050a35c01a4552515437b1058bb3ce00540e1
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-22 21:25:40 +0200

    Update genesis.py

--- genesis.py
+++ genesis.py
@@ -53,7 +53,7 @@ def create_input_script(psz_timestamp):
   #use OP_PUSHDATA1 if required
   if len(psz_timestamp) > 76: psz_prefix = '4c'
     
-  script_prefix = '04ffff001d0104' + psz_prefix + codecs.encode(ord(len(psz_timestamp)), 'hex')
+  script_prefix = '04ffff001d0104' + psz_prefix + codecs.encode(b'chr(len(psz_timestamp)'), 'hex')
   print (script_prefix + psz_timestamp.encode().hex())
   decoder = bytes.fromhex(script_prefix + psz_timestamp.encode().hex()).decode('utf-16')
   return decoder
